c02a6f7557b3665c4541ca7613419b8edd4cde26
zurajoestar <cristinal2@learn.cssd.ab.ca>
2018-12-07 13:31:35 -0700

    Up to video 10

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Cristina Lopez on 2018-12-06.
+//  Copyright © 2018 Cristina Lopez. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
